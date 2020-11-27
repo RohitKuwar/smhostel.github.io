8e2ebf30298a59e79ccbbb892e78d45b482f40ea
RohitKuwar <70262783+RohitKuwar@users.noreply.github.com>
2020-11-27 17:27:28 +0530

    Update formvalidation.js

--- registration/js/formvalidation.js
+++ registration/js/formvalidation.js
@@ -4,7 +4,7 @@ function validation1() {
     var upattern = /^[A-Za-z]{2,20}$/;
 
     if (upattern.test(first)) {
-        document.getElementById('fname').style = "background-color:green;color:yellow";
+        document.getElementById('fname').style = "color:white;opacity:0.8";
     } else {
         document.getElementById('fname').style.backgroundColor = 'red';
     }
@@ -17,7 +17,7 @@ function validation2() {
     var upattern = /^[A-Za-z]{2,20}$/;
 
     if (upattern.test(last)) {
-        document.getElementById('lname').style = "background-color:green;color:yellow";
+        document.getElementById('lname').style = "color:white;opacity:0.8";
     } else {
         document.getElementById('lname').style.backgroundColor = 'red';
     }
@@ -30,7 +30,7 @@ function funmail() {
     var upattern = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,}$/;
 
     if (upattern.test(mail)) {
-        document.getElementById('email').style = "background-color:green;color:yellow";
+        document.getElementById('email').style = "color:white;opacity:0.8";
     } else {
         document.getElementById('email').style.backgroundColor = 'red';
     }
@@ -42,9 +42,30 @@ function funmobile() {
     var upattern = /^[0-9]{10}$/;
 
     if (upattern.test(mob)) {
-        document.getElementById('mobile').style = "background-color:green;color:yellow";
+        document.getElementById('mobile').style = "color:white;opacity:0.8";
     } else {
         document.getElementById('mobile').style.backgroundColor = 'red';
     }
 
 }
+
+// function myalert(){
+
+//     var first = document.getElementById('fname').value;
+//     var last = document.getElementById('lname').value;
+//     var mail = document.getElementById('email').value;
+//     var mob = document.getElementById('mobile').value;
+
+//     if (first !== '' && last !== '' && mail !== '' && mob !== '') {
+        // alert('Congratulations!!! Your information has been registered succesfully & we will contact you soon');
+//         swal("Congratulations!!!", "Your information has been registered succesfully & we will contact you soon.", "success");
+//     }
+// }
+
+// function myalert() {
+//     if(validation1 && validation2 && funmobile && funmail) {
+//         swal("Congratulations!!!", "Your information has been registered succesfully & we will contact you soon.", "success");
+//     }
+// }
+// setTimeout(myalert, 1000);
+
